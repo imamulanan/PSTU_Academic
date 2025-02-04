2435a3ed8ff09be52c4caf91d5eb5d4812e33667
Imamul Kabir Anan <135373964+imamulanan@users.noreply.github.com>
2025-02-04 21:34:55 +0600

    Add files via upload

---
+++ Semester 4/MySQL/Inner Exclusive Join.sql
@@ -0,0 +1,30 @@
+create database Inner_Exclusive_Join;
+use Inner_Exclusive_Join;
+
+create table student (
+        roll_no int primary key,
+        name varchar(50)
+ );
+ 
+ insert into student values (101,"Anan"),(102,"Miad"),
+ (103,"Miraj");
+ 
+ create table course (
+        roll_no int primary key,
+        course varchar(50)
+ );
+ 
+insert into course values (102,"CIT"),(105,"MAT"),
+ (103,"CCE"),(107,"EEE"); 
+ 
+
+
+select * from student as s
+left join course as c
+on s.roll_no = c.roll_no
+where c.roll_no is null
+union
+select * from student as s
+right join course as c
+on s.roll_no = c.roll_no
+where s.roll_no is null; 

cb9d3d4719a5a82cbf5ceba3abb1e98ec20fb27c
Imamul Kabir Anan <135373964+imamulanan@users.noreply.github.com>
2025-02-18 01:14:22 +0600

    Add files via upload

---
+++ Semester 4/MySQL/Chap03_Attribute_Specification.sql
@@ -0,0 +1,32 @@
+SELECT instructor. * FROM instructor, teaches WHERE instructor.ID=teaches.ID;
+
+
+
+ID	name	dept_name	salary	
+14365	Lembr	Accounting	32241.56	
+14365	Lembr	Accounting	32241.56	
+15347	Bawa	Athletics	72140.88	
+19368	Wieland	Pol. Sci.	124651.41	
+19368	Wieland	Pol. Sci.	124651.41	
+19368	Wieland	Pol. Sci.	124651.41	
+22591	DAgostino	Psychology	59706.49	
+22591	DAgostino	Psychology	59706.49	
+22591	DAgostino	Psychology	59706.49	
+22591	DAgostino	Psychology	59706.49	
+22591	DAgostino	Psychology	59706.49	
+22591	DAgostino	Psychology	59706.49	
+22591	DAgostino	Psychology	59706.49	
+22591	DAgostino	Psychology	59706.49	
+22591	DAgostino	Psychology	59706.49	
+22591	DAgostino	Psychology	59706.49	
+22591	DAgostino	Psychology	59706.49	
+22591	DAgostino	Psychology	59706.49	
+22591	DAgostino	Psychology	59706.49	
+25946	Liley	Languages	90891.69	
+28097	Kean	English	35023.18	
+28097	Kean	English	35023.18	
+28400	Atanassov	Statistics	84982.92	
+28400	Atanassov	Statistics	84982.92	
+3199	Gustafsson	Elec. Eng.	82534.37	
+
+
